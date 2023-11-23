ccc9bcf71398da4aa6118d2e4b6644db317f5a29
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-11-23 16:52:36 +0300

    new

--- school/settings.py
+++ school/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'school-system',
+#         'USER': 'root',
+#         'PASSWORD': 'oneBitcoin=7000$',
+#         'PORT': 3306,
+#         'HOST': 'localhost'
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'school-system',
-        'USER': 'root',
-        'PASSWORD': 'oneBitcoin=7000$',
-        'PORT': 3306,
-        'HOST': 'localhost'
-    }
-}
-"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 
 # Password validation
